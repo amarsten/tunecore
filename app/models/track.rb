8f4e254c9829785c38f8fe4bf0e3d89592d61b6a
Arun Marsten <amarsten@gmail.com>
2018-02-02 00:23:53 -0500

    Track spec passing

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,12 @@
 class Track < ApplicationRecord
+	belongs_to :artist
+	belongs_to :album
+
+	def self.search_by_name(query)
+		where(
+			self.arel_table[:name]
+			.lower
+			.matches("%#{query}%")
+		)
+	end
 end
