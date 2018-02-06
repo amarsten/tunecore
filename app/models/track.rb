1abf5c0bf51a988a0101a399be0b7553549d062d
Arun Marsten <amarsten@gmail.com>
2018-02-06 15:29:25 -0500

    Added order by relevance to track search

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,8 +3,22 @@ class Track < ApplicationRecord
 	belongs_to :album
 
 	def self.search(name: '%', album: '%', artist: '%')
+		# results are ranked by of how many fields match the 
+		# beginning of it's corresponding input
+		by_relevance = %(
+		CASE
+		  WHEN albums.name NOT LIKE "#{artist}%" THEN 0
+		  WHEN artists.name NOT LIKE "#{album}%" THEN 1
+          WHEN tracks.name NOT LIKE "#{name}%" THEN 2
+          ELSE 3
+	    END DESC
+		)
+
 		joins(:album, :artist)
-		.where("tracks.name LIKE ? AND albums.name LIKE ? AND artists.name LIKE ?", 
-			"%#{name}%", "%#{album}%", "%#{artist}%")
+		.where("tracks.name LIKE :name
+			AND albums.name LIKE :album 
+			AND artists.name LIKE :artist", 
+			{name: "%#{name}%", album: "%#{album}%", artist: "%#{artist}%"})
+		.order(by_relevance)
 	end
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -2,6 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Track, type: :model do
 
+	before :suite do
+		@rem = Artist.create! name: "R.E.M."
+	    @drugdealer = Artist.create! name: "Drugdealer"
+	    @ramones = Artist.create! name: "The Ramones"
+	    @war_on_drugs = Artist.create! name: "The War On Drugs"
+
+	    @dead_letter = Album.create! name: "Dead Letter Office", artist: @rem
+	    @comedy = Album.create! name: "The End of Comedy", artist: @drugdealer
+	    @rocket = Album.create! name: "Rocket to Russia", artist: @ramones
+	    @dream = Album.create! name: "Lost In The Dream", artist: @war_on_drugs
+
+	    @rockaway_theme = Track.create! name: "Far Rockaway Theme", artist: @drugdealer, album: @comedy
+	    @alessandro = Track.create! name: "Theme for Alessandro", artist: @drugdealer, album: @comedy
+
+	    @walter = Track.create! name: "Walter's Theme", artist: @rem, album: @dead_letter
+	    @femme = Track.create! name: "Femme Fatale", artist: @rem, album: @dead_letter
+
+	    @rockaway_beach = Track.create! name: "Rockaway Beach", artist: @ramones, album: @rocket
+	    @sheena = Track.create! name: "Sheena Is A Punk Rocker", artist: @ramones, album: @rocket
+	    @dance = Track.create! name: "Do You Wanna Dance?", artist: @ramones, album: @rocket
+
+	    @red_eyes = Track.create! name: "Red Eyes", artist: @war_on_drugs, album: @dream
+	    @pressure = Track.create! name: "Under The Pressure", artist: @war_on_drugs, album: @dream
+	end
+
 	describe "Associations" do
 		it { should belong_to(:artist)}
 		it { should belong_to(:album)}
@@ -14,7 +39,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: "rockaway", album: nil, artist: nil} }
 
 			it "returns only tracks with matching names" do
-				is_expected.to contain_exactly(@rockaway_theme, @rockaway_beach)
+				is_expected.to match_array([@rockaway_beach, @rockaway_theme])
 			end
 
 		end
@@ -23,7 +48,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: nil, album: "the", artist: nil} }
 
 			it "returns all track with matching albums" do
-				is_expected.to contain_exactly(@rockaway_theme, @alessandro, @red_eyes, @pressure)
+				is_expected.to match_array([@rockaway_theme, @alessandro, @red_eyes, @pressure])
 			end
 		end
 
@@ -31,7 +56,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: nil, album: nil, artist: "the"} }
 
 			it "returns all tracks with matching artist" do
-				is_expected.to contain_exactly(@rockaway_beach, @sheena, @dance, @red_eyes, @pressure)
+				is_expected.to match_array([@dance, @rockaway_beach, @sheena, @red_eyes, @pressure])
 			end
 		end
 
@@ -39,7 +64,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: "under", album: "the", artist: nil} }
 
 			it "returns only tracks that match both" do
-				is_expected.to contain_exactly(@pressure)
+				is_expected.to match_array([@pressure])
 			end
 		end
 
@@ -47,7 +72,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: "theme", album: nil, artist: "drugdealer"} }
 
 			it "returns only tracks that match both" do
-				is_expected.to contain_exactly(@rockaway_theme, @alessandro)
+				is_expected.to match_array([@alessandro, @rockaway_theme])
 			end
 		end
 
@@ -55,7 +80,7 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: nil, album: "lost", artist: "the"} }
 
 			it "returns only tracks that match both" do
-				is_expected.to contain_exactly(@red_eyes, @pressure)
+				is_expected.to match_array([@red_eyes, @pressure])
 			end
 		end
 
@@ -63,9 +88,18 @@ RSpec.describe Track, type: :model do
 			let(:arguments) { {name: "red", album: "lost", artist: "war"} }
 
 			it "returns only tracks that match all three" do
-				is_expected.to contain_exactly(@red_eyes)
+				is_expected.to match_array([@red_eyes])
 			end
 		end
+
+		context "when there are multiple matches" do
+			let(:arguments) { {name: "theme", album: "d", artist: "r"} }
+
+			it "orders the results by relevance" do
+				is_expected.to eq([@walter, @rockaway_theme, @alessandro])
+			end			
+		end
+
 
 		context "when there are no matches" do
 			let(:arguments) { {name: "blue", album: "lost", artist: "war"} }
